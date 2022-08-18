692417b1e584a8fc9221b2decf1d0b1908be2229
Derrick Hammer <derrick@derrickhammer.com>
2022-08-18 14:03:04 -0400

    *Add helper functions for handling resolver responses

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { DNSRecord, DNSResult } from "./types.js";
+
 export function getTld(domain: string): string {
   if (domain.includes(".")) {
     domain = domain.split(".")[domain.split(".").length - 1];
@@ -27,4 +29,25 @@ export function isDomain(domain: string) {
   return /(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]/.test(
     domain
   );
+}
+export function resolverEmptyResponse(): DNSResult {
+  return {
+    records: [],
+  };
+}
+
+export function resolverError(e: Error | string): DNSResult {
+  if (!(e instanceof Error)) {
+    e = new Error(e);
+  }
+  return {
+    records: [],
+    error: e,
+  };
+}
+
+export function resolveSuccess(records: DNSRecord[]): DNSResult {
+  return {
+    records,
+  };
 }
