09a5a605a7daa7320e76e72d82c126622c6678eb
Derrick Hammer <derrick@derrickhammer.com>
2022-08-19 15:45:08 -0400

    *Update dist

--- dist/resolverModule.d.ts
+++ dist/resolverModule.d.ts
@@ -1,23 +1,26 @@
 import type { DNSResult, ResolverOptions } from "./types.js";
-import { ResolverRegistry } from "@lumeweb/resolver";
+import { RpcNetwork } from "@lumeweb/dht-rpc-client";
+declare class ResolverRegistry {
+    private _resolvers;
+    private _rpcNetwork;
+    constructor(network?: RpcNetwork);
+    get resolvers(): Set<ResolverModule>;
+    get rpcNetwork(): RpcNetwork;
+    resolve(domain: string, options?: ResolverOptions, bypassCache?: boolean): Promise<DNSResult>;
+    register(resolver: ResolverModule): void;
+    clear(): void;
+}
 export interface ResolverModule {
-  constructor(resolver: ResolverRegistry): any;
-  resolve(
-    domain: string,
-    options: ResolverOptions,
-    bypassCache: boolean
-  ): Promise<DNSResult>;
-  getSupportedTlds(): string[];
+    constructor(resolver: ResolverRegistry): any;
+    resolve(domain: string, options: ResolverOptions, bypassCache: boolean): Promise<DNSResult>;
+    getSupportedTlds(): string[];
 }
 export declare abstract class AbstractResolverModule {
-  protected resolver: ResolverRegistry;
-  constructor(resolver: ResolverRegistry);
-  abstract resolve(
-    domain: string,
-    options: ResolverOptions,
-    bypassCache: boolean
-  ): Promise<DNSResult>;
-  getSupportedTlds(): string[];
-  isTldSupported(domain: string): boolean;
+    protected resolver: ResolverRegistry;
+    constructor(resolver: ResolverRegistry);
+    abstract resolve(domain: string, options: ResolverOptions, bypassCache: boolean): Promise<DNSResult>;
+    getSupportedTlds(): string[];
+    isTldSupported(domain: string): boolean;
 }
+export {};
 //# sourceMappingURL=resolverModule.d.ts.map
