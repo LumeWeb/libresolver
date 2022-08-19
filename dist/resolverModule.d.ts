234074fb8f5b74b918df10db3b99b2588bc16d89
Derrick Hammer <derrick@derrickhammer.com>
2022-08-19 15:49:56 -0400

    *Update dist

--- dist/resolverModule.d.ts
+++ dist/resolverModule.d.ts
@@ -1,26 +1,38 @@
 import type { DNSResult, ResolverOptions } from "./types.js";
-import { RpcNetwork } from "@lumeweb/dht-rpc-client";
+import type { RpcNetwork } from "@lumeweb/dht-rpc-client";
 declare class ResolverRegistry {
-    private _resolvers;
-    private _rpcNetwork;
-    constructor(network?: RpcNetwork);
-    get resolvers(): Set<ResolverModule>;
-    get rpcNetwork(): RpcNetwork;
-    resolve(domain: string, options?: ResolverOptions, bypassCache?: boolean): Promise<DNSResult>;
-    register(resolver: ResolverModule): void;
-    clear(): void;
+  private _resolvers;
+  private _rpcNetwork;
+  constructor(network?: RpcNetwork);
+  get resolvers(): Set<ResolverModule>;
+  get rpcNetwork(): RpcNetwork;
+  resolve(
+    domain: string,
+    options?: ResolverOptions,
+    bypassCache?: boolean
+  ): Promise<DNSResult>;
+  register(resolver: ResolverModule): void;
+  clear(): void;
 }
 export interface ResolverModule {
-    constructor(resolver: ResolverRegistry): any;
-    resolve(domain: string, options: ResolverOptions, bypassCache: boolean): Promise<DNSResult>;
-    getSupportedTlds(): string[];
+  constructor(resolver: ResolverRegistry): any;
+  resolve(
+    domain: string,
+    options: ResolverOptions,
+    bypassCache: boolean
+  ): Promise<DNSResult>;
+  getSupportedTlds(): string[];
 }
 export declare abstract class AbstractResolverModule {
-    protected resolver: ResolverRegistry;
-    constructor(resolver: ResolverRegistry);
-    abstract resolve(domain: string, options: ResolverOptions, bypassCache: boolean): Promise<DNSResult>;
-    getSupportedTlds(): string[];
-    isTldSupported(domain: string): boolean;
+  protected resolver: ResolverRegistry;
+  constructor(resolver: ResolverRegistry);
+  abstract resolve(
+    domain: string,
+    options: ResolverOptions,
+    bypassCache: boolean
+  ): Promise<DNSResult>;
+  getSupportedTlds(): string[];
+  isTldSupported(domain: string): boolean;
 }
 export {};
 //# sourceMappingURL=resolverModule.d.ts.map
--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,25 +1,25 @@
 export interface ResolverOptions {
-    type: string;
-    customType?: string;
-    options?: any;
+  type: string;
+  customType?: string;
+  options?: any;
 }
 export interface DNSResult {
-    records: DNSRecord[];
-    error?: Error;
+  records: DNSRecord[];
+  error?: Error;
 }
 export interface DNSRecord {
-    type: string;
-    customType?: string;
-    value: string;
+  type: string;
+  customType?: string;
+  value: string;
 }
 declare const DNS_RECORD_TYPE: {
-    A: string;
-    CNAME: string;
-    NS: string;
-    CONTENT: string;
-    TEXT: string;
-    ALL: string;
-    CUSTOM: string;
+  A: string;
+  CNAME: string;
+  NS: string;
+  CONTENT: string;
+  TEXT: string;
+  ALL: string;
+  CUSTOM: string;
 };
 export { DNS_RECORD_TYPE };
 //# sourceMappingURL=types.d.ts.map
